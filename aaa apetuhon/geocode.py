83bcfe0d91ed27350c06431f9a720c4584c6453d
dast3rr <129662556+dast3rr@users.noreply.github.com>
2024-07-24 14:10:08 +0300

    Delete aaa apetuhon/geocode.py

--- aaa apetuhon/geocode.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-def get_cords(point, API_KEY):
-    response = requests.get('https://catalog.api.2gis.com/3.0/items/geocode?q={point}&fields=items.point&key={API_KEY}')
-    # return response['result']['items']['point']
